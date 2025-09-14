e6a29009e2ebd2fa1c6aa629ab200ec922346bf4
serendipist <tovkfksxpfks@gmail.com>
2025-09-14 18:39:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// app.js - 카테고리 수정 문제 디버깅 및 API 연동
-
-// ▼▼▼ 선생님의 실제 앱스 스크립트 웹앱 배포 URL로 반드시 교체해주세요!!! ▼▼▼
-const APPS_SCRIPT_API_ENDPOINT = "https://script.google.com/macros/s/AKfycbxiR6w6tIohFbPzTgAAVZtHeJqtyMyzVlMndDib0PB9x9W9k_282UpZ3zr19vkK2l8/exec"; //https://github.com/serendipist/my-budget-app/blob/main/app.js
-// ▲▲▲ 선생님의 실제 배포 URL을 다시 한번 확인해주세요. ▲▲▲
-
 /* === 전역 상태 === */
 let currentDisplayDate = new Date();
 let currentCycleMonth = '';
@@ -734,6 +728,7 @@ function updateSubCategories() {
     });
   }
 }
+
 
 
 
